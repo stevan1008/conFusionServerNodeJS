425777bf4f184ec15d57052f57586ecc444b6f54
stevan1008 <stevan1008@hotmail.com>
2021-10-28 23:24:11 -0500

    express session part 1

--- app.js
+++ app.js
@@ -1,58 +1,120 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes');
+const Dishes = require("./models/dishes");
+const Promos = require("./models/promotions");
+const Leaders = require("./models/leaders");
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-  console.log("Connected correctly to server");
-}, (err) => { console.log(err); })
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser("0000-1111-2222-3333"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use(session({
+  name: "session-id",
+  secret: "0000-1111-2222-3333",
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
+
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      var err = new Error("You are not authenticated!");
+
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+
+    var username = auth[0];
+    var password = auth[1];
+    if (username === "admin" && password === "password") {
+      req.session.user = "admin";
+      next(); //Ha sido autorizado
+    } else {
+      var err = new Error("You are not authenticated!");
+
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+  } else {
+    if (req.session.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -57,7 +57,7 @@ dishRouter.route('/:dishId')
 
 .post((req, res, next) => {
     res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/:dishId' + req.params.dishId);
+    res.end('POST operation not supported on /dishes/' + req.params.dishId);
 })
 
 .put((req, res, next) => {
